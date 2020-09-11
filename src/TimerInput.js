15772c8d1ef44508ce35bd0a23613767242106a5
pigeotto <dapigeotto@gmail.com>
2020-09-12 01:45:36 +0700

    Lodash Debounce Example

--- src/TimerInput.js
+++ src/TimerInput.js
@@ -1,24 +1,29 @@
 import React, { useState, useEffect } from 'react';
 import Form from "react-bootstrap/Form";
+import _ from "lodash";
 
 const TimerInput = (props) => {
   
-  const [value, setValue] = useState(props.value);
-  const [valid, setValid] = useState(true);
+	const [value, setValue] = useState(props.value);
+	const [valid, setValid] = useState(true);
 	const [readonly, setReadonly] = useState(false);
-	useEffect(()=>{
-    
-    /*TODO: */
-    setReadonly(props.startCountdown);
-    if (props.startCountdown) {
-			if (value > 0) {
-        setValid(true);
-				setTimeout(()=>setValue((value-0.1)), 100);
-			}else if (!value || isNaN(value)) {
-        setValid(false);
-      }
-		}
 
+	/*Example of Lodash Debounce*/
+	// const setValueOnWait = _.debounce((value) => {
+	// 	return setValue(value);
+	// }, 500);
+
+	useEffect(()=>{
+	    /*TODO: */
+	    setReadonly(props.startCountdown);
+	    if (props.startCountdown) {
+				if (value > 0) {
+	        		setValid(true);
+					setTimeout(()=>setValue((value-0.1)), 100);
+				}else if (!value || isNaN(value)) {
+	        		setValid(false);
+	      		}
+			}
 	})
 
     return (
